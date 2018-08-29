84646ef871483b8e855862af4fead3c6d8f4722b
czamp <clay.zamperini@gmail.com>
2018-08-29 15:03:58 -0700

    Delete CRA CSS and remove Bulma as dependency.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import rootReducer from './reducers'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 
-const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
+const store = createStore(
+  rootReducer,
+  window.devToolsExtension && window.devToolsExtension()
+);
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>
-  , document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
